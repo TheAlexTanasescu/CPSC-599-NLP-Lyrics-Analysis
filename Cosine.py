1677780ce838169bf7a19aba7eaec82ecbb669cb
Abhinav Ghai <60748818+ClownEmojiIsHumor@users.noreply.github.com>
2023-04-07 19:18:59 -0600

    Updated to clean it a bit better

--- Cosine.py
+++ Cosine.py
@@ -5,6 +5,10 @@ from gensim.models.doc2vec import Doc2Vec
 from sklearn.metrics.pairwise import cosine_similarity
 import ssl
 from scipy.spatial.distance import cosine
+from nltk.corpus import stopwords
+import string
+from nltk.stem import WordNetLemmatizer
+from collections import Counter
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -12,6 +16,62 @@ ssl._create_default_https_context = ssl._create_unverified_context
 # Download necessary NLTK data
 nltk.download('punkt')
 nltk.download('stopwords')
+
+stop_words = set(stopwords.words('english'))
+
+#Regex to remove things inside of [] and the [] themselves
+def remove_brackets(text):
+    cleaned_text = re.sub(r'\[[^]]*\]|\([^)]*\)', '', text)
+    return cleaned_text
+
+def remove_break_chars(text):
+    cleaned_text = re.sub(r'[\n\t]', ' ', text)
+    return cleaned_text
+
+def remove_regex(text):
+    cleaned_text = remove_break_chars(remove_brackets(text))
+    return cleaned_text
+
+def remove_punctuation(text):
+    # Remove punctuation from the text
+    return text.translate(str.maketrans('', '', string.punctuation))
+
+def tokenize(text):
+    # Tokenize the text into individual words using nltk
+    return nltk.word_tokenize(text)
+
+def lemmatize(tokens):
+    # Lemmatize the tokens using nltk
+    lemmatizer = WordNetLemmatizer()
+    return [lemmatizer.lemmatize(token).lower() for token in tokens if len(token) > 2]
+
+def remove_top_tokens(tokens, percent):
+    # Count the frequency of each token
+    token_counts = Counter(tokens)
+    
+    # Calculate the number of tokens to remove (top percent%)
+    num_tokens_to_remove = int(len(token_counts) * (percent / 100))
+    
+    # Remove the top n tokens from the list of tokens
+    top_tokens = [token for token, count in token_counts.most_common(num_tokens_to_remove)]
+    return [token for token in tokens if token not in top_tokens and token not in stop_words]
+
+def clean_and_tokenize_corpus(corpus):
+    # Remove punctuation from the text
+    corpus = remove_punctuation(corpus)
+    
+    # Tokenize the text into individual words using nltk
+    tokens = tokenize(corpus)
+    
+    # Lemmatize the tokens using nltk
+    tokens = lemmatize(tokens)
+    
+    # Remove the top 1% of tokens
+    tokens = remove_top_tokens(tokens, 1)
+    
+    return tokens
+
+
 
 # Function to clean the lyrics
 def clean_lyrics(lyrics):
@@ -39,10 +99,11 @@ while (1):
     if nl == "`" : break
     S_L = S_L + nl
 
-lyrics = S_L    
+lyrics = S_L  
+print(S_L)
 
 # Clean the lyrics
-lyrics = clean_lyrics(lyrics)
+lyrics = clean_and_tokenize_corpus(lyrics)
 
 #print(sentences)
 # Load the pre-trained Doc2Vec model
@@ -80,17 +141,17 @@ sim1 = []
 for genre, embedding in genre_embeddings.items():
     cos_sim = cosine(new_embedding, embedding)
     # cos_sim1 = cosine_similarity([new_embedding], [embedding])[0][0]
-    similarities.append((genre, cos_sim))
+    similarities.append((genre,  cos_sim))
     # sim1.append((genre, cos_sim1))
 
 # print(similarities)
 
 #Sort by highest
 similarities.sort(key = lambda x: x[1], reverse=False)
-# sim1.sort(key = lambda x: x[1], reverse= False)
+# sim1.sort(key = lambda x: x[1], reverse= True)
 
 
-num_closest = 5
+num_closest = 10
 
 # Print the closest embeddings
 for i in range (num_closest):
